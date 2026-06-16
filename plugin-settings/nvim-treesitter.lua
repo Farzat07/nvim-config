e21e4961a8b4a00bfaa9abf4a4df30bf4ebba1e7
A Farzat <a@farzat.xyz>
2026-06-16 16:21:32 +0300

    Add vimdoc to the default treesitter languages

--- plugin-settings/nvim-treesitter.lua
+++ plugin-settings/nvim-treesitter.lua
@@ -1,1 +1,1 @@
-require('nvim-treesitter').install({ 'c', 'lua', 'markdown', 'vim' }):wait(300000) -- wait max. 5 minutes
+require('nvim-treesitter').install({ 'c', 'lua', 'markdown', 'vim', 'vimdoc' }):wait(300000) -- wait max. 5 minutes
